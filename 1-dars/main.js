150a83d6f544380e2c48b03e36c854a13e5559ad
ozodbek090 <itmars047@gmail.com>
2025-09-18 09:04:58 +0500

    Add files via upload

---
+++ 1-dars/main.js
@@ -0,0 +1,15 @@
+const num1 = prompt ("ismigizni kiriting  VA  familigizni kiriting");
+const num2 = +prompt ("yoshgizni kiriting");
+const num3 = prompt ("yoqtirgan fanigizni yozig");
+const num4 = prompt ("yoqtirgan tumanizni kiriting");
+const num5 = alert ("raxmat kiritkaushn");
+
+
+
+
+console.log(`Ism va Familiea:`,num1);
+console.log(`Yosh:`,num2);
+console.log(`Yoqtirgan fan:`,num3);
+console.log(`Yoqtirgan manzili:`,num4);
+
+
